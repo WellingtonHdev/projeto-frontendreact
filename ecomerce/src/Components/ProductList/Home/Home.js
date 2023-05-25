fceb40d426bf243d2287c2696a2966aff53f23fa
WellingtonHdev <wellingtontmv@gmail.com>
2023-05-25 22:36:20 +0100

    [funcionalidade] apliquei Inputs controlados no select

--- ecomerce/src/Components/ProductList/Home/Home.js
+++ ecomerce/src/Components/ProductList/Home/Home.js
@@ -1,14 +1,24 @@
+import { useState } from "react"
 import ProductCard from "../ProductCard/ProductCard"
 import { HomeStyle } from "./HomeStyle"
 import { Container } from "./HomeStyle"
 import { Header } from "./HomeStyle"
 
 export default function Home({ productList }) {
+
+  const [ordination, setOrdination] = useState("");
+
+  function onChangeSelect (){
+
+  } 
+
   return (
     <HomeStyle>
       <Header>
         <spam>Quantidade de produtos: {productList.length} </spam>
-        <spam>Ordenar:<select>
+        <spam>Ordenar:
+          <select value={ordination} onChange={()=>onChangeSelect()}>
+            <option></option>
           <option>Crescente</option>
           <option>Decrescente</option>
         </select></spam>
